37c09e48adf186748003ccc247c0bf7308be6b09
bibhusprasad <bibhusprasad@gmail.com>
2025-11-16 12:17:12 +0530

    02. Another Component in React

--- react-basic/src/FirstComponent.jsx
+++ react-basic/src/FirstComponent.jsx
@@ -3,7 +3,23 @@ function FirstComponent() {
   return (
     <div>
       <h2>First Component in React</h2>
+      <Fruit />
+      <Color />
     </div>
+  )
+}
+
+//Also a component in React
+function Fruit() {
+  return(
+    <h3>Apple</h3>
+  )
+}
+
+//Also a component in React
+function Color() {
+  return(
+    <h3>Red</h3>
   )
 }
 
